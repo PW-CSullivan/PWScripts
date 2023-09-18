9609b19c7b23d91372845256c4f6649bb8bca22a
PW-CSullivan <118478293+PW-CSullivan@users.noreply.github.com>
2023-09-18 11:45:48 -0700

    Create PW-FormRefresh.user.js

---
+++ PW-FormRefresh.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Public Works Form Refresh
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Used to refresh the page for Public Works Laserfiche forms every 10 minutes if there is no data
+//               in the fields or if the same data is in the fields for the last 10 minutes. 
+// @author       Christopher Sullivan
+// @include      https://bc-forms/Forms/VehicleRepairRequest
+// @require      https://github.com/PW-CSullivan/PWScripts/raw/master/SearchElements.js
+// @downloadURL  https://github.com/PW-CSullivan/PWScripts/raw/master/PW-FormRefresh.user.js
+// @updateURL    https://github.com/PW-CSullivan/PWScripts/raw/master/PW-FormRefresh.user.js
+// @run-at document-idle
+// @grant        none
+// ==/UserScript==
+
+// Main function that runs on page load.
+(function () {
+	var old_values = getFieldData();
+	// Runs once every 10 minutes.
+	var check_run = setInterval(function () {
+		var values = getFieldData();
+		var different = false;
+		if (old_values.length == values.length) {
+			for (var i = 0; i < old_values.length; i++) {
+				if (old_values[i] != values[i]) {
+					different = true;
+					break;
+				}
+			}
+		}
+		if (!different) {
+			window.location.reload();
+		} else {
+			different = false;
+			old_values = getFieldData();
+		}
+	}, 600000);
+});
+
+function getFieldData() {
+	var texts = [];
+	var fields = findAll("input");
+	for (var i = 0; i < fields.length; i++) {
+		if (fields.type != "hidden" && !fields[i].readOnly) {
+			texts.push(fields[i].value);
+		}
+	}
+	return texts;
+}
