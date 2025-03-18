c792cfa8fb47aa453d9b3192833db63c353ebb46
PW-CSullivan <118478293+PW-CSullivan@users.noreply.github.com>
2025-03-18 08:59:04 -0700

    Update Waiter2.js
    Made the waiter script use cookies now

--- Waiter2.js
+++ Waiter2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Waiter2
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      2.0.0
 // @description  Used by my other scripts to do automation
 // @author       Christopher Sullivan
 // @match        *
@@ -18,11 +18,13 @@ function Waiter() {
     this.table_time = [];
     this.addSingle = function(name, orderCheck, check_time=1500) {
         this.single_name.push(name);
+        setCookieArray("single_name", this.single_name);
         this.single_list.push(setInterval(function() {
             orderCheck();
         }, check_time));
     };
     this.clearSingle = function(name) {
+        this.single_name = getCookieArray("single_name");
         var index = -1;
         for (var i = 0; i < this.single_name.length; i++) {
             if (this.single_name[i] == name) {
@@ -43,7 +45,9 @@ function Waiter() {
         if (title != '') {
             console.log("Title:", title);
         }
+        this.single_name = getCookieArray("single_name");
         console.log('Single Amount:', this.single_list.length);
+        console.log('Single Cookie Amount:', this.single_name.length);
         for (var i = 0; i < this.single_name.length; i++) {
             if (this.clearSingle(this.single_name[i])) {
                 check = true;
@@ -52,17 +56,23 @@ function Waiter() {
         if (!check) {
             this.single_list = [];
             this.single_name = [];
+            setCookieArray("single_name", this.single_name);
         }
         console.log('---------------------------');
     };
     this.addTable = function(orderCheck, check_time=250, clearCondition=false, timer_total=60000) {
+        this.waiting_list = getCookieArray("waiting_list");
         var table_number = this.waiting_list.length;
         this.table_list.push(false);
         this.table_time.push(0);
+        setCookieArray("table_list", this.table_list);
+        setCookieArray("table_time", this.table_time);
         this.waiting_list.push(setInterval(this.checkTable, check_time, table_number, orderCheck, clearCondition, check_time, timer_total, this));
         return this.waiting_list.length - 1; // Returns current index
     };
     this.checkTable = function(table_number, orderCheck, clearCondition, check_time, timer_total, myWaiter) {
+        myWaiter.table_time = getCookieArray("table_time");
+        myWaiter.table_list = getCookieArray("table_list");
         if (myWaiter.table_time[table_number] <= timer_total) {
             myWaiter.table_time[table_number] += check_time;
             if (clearCondition == false) {
@@ -91,10 +101,14 @@ function Waiter() {
         }
     };
     this.clearTable = function(table_number) {
+        this.table_list = getCookieArray("table_list");
+        this.waiting_list = getCookieArray("waiting_list");
         if (table_number < this.table_list.length &&
             table_number < this.waiting_list.length) {
             this.table_list[table_number] = true;
             this.table_time[table_number] = 0;
+            setCookieArray(this.table_list);
+            setCookieArray(this.table_time);
             clearInterval(this.waiting_list[table_number]);
         }
     };
@@ -104,6 +118,7 @@ function Waiter() {
         }
     };
     this.tableClearBefore = function(table_number) {
+        this.table_list = getCookieArray("table_list");
         for (var i = 0; i <= table_number; i++) {
             if (!this.table_list[i]) {
                 return false;
@@ -114,15 +129,19 @@ function Waiter() {
     this.clearAllTables = function() {
         console.log('-----Clear All Tables-----');
         console.log('Amount of Tables:', this.amountOfTables());
+        this.waiting_list = getCookieArray("waiting_list");
         for (var i = 0; i < this.waiting_list.length; i++) {
             clearInterval(this.waiting_list[i]);
         }
         this.waiting_list = [];
         this.table_list = [];
         this.table_time = [];
+        setCookieArray("waiting_list", this.waiting_list);
+        setCookieArray("table_list", this.table_list);
+        setCookieArray("table_time", this.table_name);
     };
     this.amountOfTables = function() {
-        return this.waiting_list.length;
+        return getCookie("waiting_list.length");
     };
     this.isEmpty = function() {
         if (this.amountOfTables() > 0) {
@@ -143,5 +162,107 @@ function Waiter() {
             }
         }
         return false;
+    };
+}
+
+/**
+ * setCookie
+ * a function that sets a cookie value for a default of 1 day to the current page. 
+ * 
+ * @param cname - Name of the cookie
+ * @param cvalue - Value of the cookie
+ * @param exdays - How long until expires
+ */
+function setCookie(cname, cvalue, exdays=1) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/**
+ * setCookieArray
+ * A function that sets several cookies for each value in the array.
+ * Sets a cookie of cname called "array" to check if it's array.
+ * Sets a cookie length of array cname + ".length"
+ * 
+ * @param cname - base name of array
+ * @param carray - the Array to put in the cookies
+ * @param exdays - how long until expires
+ */
+function setCookieArray(cname, carray, exdays=1) {
+    if (Array.isArray(carray)) {
+        setCookie(cname, "array", exdays);
+        setCookie((cname + ".length"), carray.length, exdays);
+        for (let i = 0; i < carray.length; i++) {
+            setCookie((cname + "." + i.toString()), carray[i], exdays);
+        }
     }
+}
+
+/**
+ * getCookie
+ * a function that searches for the given cookie name and returns the value.
+ * If the cookie isn't found returns ""
+ * 
+ * @param cname - name of cookie
+ */
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+/**
+ * getCookieArray
+ * A function that returns an array of values from cookies based 
+ * on the given cname. 
+ * 
+ * @param cname - Name of array
+ */
+function getCookieArray(cname) {
+    let blankArray = [];
+    if (checkCookieArray(cname)) {
+        let size = getCookie(cname + ".length");
+        for (let i = 0; i < size; i++) {
+            blankArray.push(getCookie(cname + "." + i.toString()));
+        }
+    }
+    return blankArray;
+}
+
+/**
+ * checkCookie
+ * A function that returns whether the cookie exists.
+ * 
+ * @param cname - Name of cookie
+ */
+function checkCookie(cname) {
+    if (getCookie(cname) != "") {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * checkCookie
+ * A function that returns whether the cookie is an array.
+ * 
+ * @param cname - Name of cookie
+ */
+function checkCookieArray(cname) {
+    if (getCookie(cname) == 'array') {
+        return true;
+    }
+    return false;
 }
