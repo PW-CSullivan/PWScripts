aff620b6ec22d188a9b5f9a1d0a28cc49a8dc3de
PW-CSullivan <118478293+PW-CSullivan@users.noreply.github.com>
2025-03-18 09:31:22 -0700

    Update SearchElements.js
    Added the ability to get and save cookies

--- SearchElements.js
+++ SearchElements.js
@@ -459,3 +459,105 @@ function runAngularTrigger(css, trigger) {
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
+
+/**
+ * setCookie
+ * a function that sets a cookie value for a default of 1 day to the current page. 
+ * 
+ * @param cname - Name of the cookie
+ * @param cvalue - Value of the cookie
+ * @param exdays - How long until expires
+ */
+function setCookie(cname, cvalue, exdays=1) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/**
+ * setCookieArray
+ * A function that sets several cookies for each value in the array.
+ * Sets a cookie of cname called "array" to check if it's array.
+ * Sets a cookie length of array cname + ".length"
+ * 
+ * @param cname - base name of array
+ * @param carray - the Array to put in the cookies
+ * @param exdays - how long until expires
+ */
+function setCookieArray(cname, carray, exdays=1) {
+    if (Array.isArray(carray)) {
+        setCookie(cname, "array", exdays);
+        setCookie((cname + ".length"), carray.length, exdays);
+        for (let i = 0; i < carray.length; i++) {
+            setCookie((cname + "." + i.toString()), carray[i], exdays);
+        }
+    }
+}
+
+/**
+ * getCookie
+ * a function that searches for the given cookie name and returns the value.
+ * If the cookie isn't found returns ""
+ * 
+ * @param cname - name of cookie
+ */
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+/**
+ * getCookieArray
+ * A function that returns an array of values from cookies based 
+ * on the given cname. 
+ * 
+ * @param cname - Name of array
+ */
+function getCookieArray(cname) {
+    let blankArray = [];
+    if (checkCookieArray(cname)) {
+        let size = getCookie(cname + ".length");
+        for (let i = 0; i < size; i++) {
+            blankArray.push(getCookie(cname + "." + i.toString()));
+        }
+    }
+    return blankArray;
+}
+
+/**
+ * checkCookie
+ * A function that returns whether the cookie exists.
+ * 
+ * @param cname - Name of cookie
+ */
+function checkCookie(cname) {
+    if (getCookie(cname) != "") {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * checkCookie
+ * A function that returns whether the cookie is an array.
+ * 
+ * @param cname - Name of cookie
+ */
+function checkCookieArray(cname) {
+    if (getCookie(cname) == 'array') {
+        return true;
+    }
+    return false;
+}
