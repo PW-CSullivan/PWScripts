ee87f3dcc23b813e4e6e1b1f0692717d8e12bcd7
PW-CSullivan <118478293+PW-CSullivan@users.noreply.github.com>
2025-04-24 08:08:29 -0700

    Update AdvancedFunctions.js

--- AdvancedFunctions.js
+++ AdvancedFunctions.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Functions
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.4
 // @description  Used with my other scripts for advanced forms of JQuery Searches
 // @author       Christopher Sullivan
 // @match        *
@@ -11,197 +11,221 @@
 // @updateURL    https://github.com/PW-CSullivan/PWScripts/raw/main/AdvancedFunctions.user.js
 // @grant        none
 // ==/UserScript==
-/**
- * find
- * function to find an element by Css selector, if element is given
- * then will search within that element for element with the css selector.
- *
- * @param selector - String that's a css selector
- * @param element - Element object from document
- * @return JQuery Object
- */
-function findSingle(selector, element=false) {
-    if (element !== false) {
-        return element.find(selector).first();
-    } else if (typeof(selector) === "string") {
-        return $(selector).first();
-    }
-    return selector.first();
+function clearAllCookies() {
+    console.log('CLEARING COOKIES');
+    allCookiesNames = getCookieArray('all-cookie-names');
+    allCookiesNames.forEach(function(c) {
+        if (checkCookieArray(c)) {
+            let len = getCookie(c + '.length');
+            for (let i = 0; i < len; i++) {
+                setCookie(c + '.' + i.toString(), '', -1);
+            }
+            setCookie(c + '.length', '', -1);
+        }
+        setCookie(c, '', -1);
+    });
+    allCookiesNames = [];
+    setCookie('all-cookie-names', '', -1);
 }
 
-/**
- * check - checks to see if an dom object exists while using
- * a css selector to find it.
- *
- * @param css - String
- * @param element - Element Object from document
- * @return boolean
- */
-function checkExist(css, element=false) {
-    if (element === false) {
-        if ($(css).length > 0) {
+function getAllCookies() {
+    let text = '';
+    allCookiesNames = getCookieArray('all-cookie-names');
+    allCookiesNames.forEach(function (c) {
+        text += '--------------------------------------------------------------\n';
+        text += c + ' = ' + getCookie(c) + '\n';
+        if (checkCookieArray(c)) {
+            let ar = getCookieArray(c);
+            for (let i = 0; i < ar.length; i++) {
+                text += c + '.' + i + ' = ' + ar[i] + '\n';
+            }
+            text += c + '.length = ' + getCookie(c + '.length') + '\n';
+        }
+    });
+    return text;
+}
+
+function addCookie(cname, cvalue, exdays=1) {
+    allCookiesNames = getCookieArray('all-cookie-names');
+    for (let i = 0; i < allCookiesNames.length; i++) {
+        if (cname == allCookiesNames[i]) {
+            setCookie(cname, cvalue, exdays);
             return true;
+        }
+    }
+    allCookiesNames.push(cname);
+    setCookie(cname, cvalue, exdays);
+    setCookieArray('all-cookie-names', allCookiesNames);
+    return false;
+}
+
+function addToCookieArray(cname, carray, exdays=1) {
+    let newArray = [];
+    if (checkCookie(cname) && getCookie(cname) == 'array') {
+        let newArray = getCookieArray(cname);
+        if (Array.isArray(carray)) {
+            carray.forEach(function (c) {
+                newArray.push(c);
+            });
+        } else {
+            newArray.push(carray);
         }
     } else {
-        if (element.find(css).length > 0) {
-            return true;
+        newArray = carray;
+    }
+    setCookieArray(cname, newArray, exdays);
+}
+
+/**
+ * setCookie
+ * a function that sets a cookie value for a default of 1 day to the current page.
+ *
+ * @param cname - Name of the cookie
+ * @param cvalue - Value of the cookie
+ * @param exdays - How long until expires
+ */
+function setCookie(cname, cvalue, exdays=1) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    cvalue = cvalue.toString();
+    cvalue = cvalue.replaceAll('=', '}');
+    cvalue = cvalue.replaceAll(';', '{');
+    if (!cname.includes('all-cookie-names')) {
+        console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+    }
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function removeCookie(cname) {
+    if (checkCookie(cname)) {
+        allCookiesNames = getCookieArray('all-cookie-names');
+        if (allCookiesNames.includes(cname)) {
+            allCookiesNames.splice(allCookiesNames.findIndex(cname), 1);
+            setCookieArray('all-cookie-names', allCookiesNames);
         }
+        setCookie(cname, '', -1);
+    }
+}
+
+function removeCookieArray(cname) {
+    if (checkCookieArray(cname)) {
+        let len = getCookie(cname + '.length');
+        for (let i = 0; i < len; i++) {
+            setCookie(cname + '.' + i.toString(), '', -1);
+        }
+        setCookie(cname + '.length', '', -1);
+        removeCookie(cname);
+    }
+}
+
+/**
+ * setCookieArray
+ * A function that sets several cookies for each value in the array.
+ * Sets a cookie of cname called "array" to check if it's array.
+ * Sets a cookie length of array cname + ".length"
+ *
+ * @param cname - base name of array
+ * @param carray - the Array to put in the cookies
+ * @param exdays - how long until expires
+ */
+function setCookieArray(cname, carray, exdays=1) {
+    if (Array.isArray(carray)) {
+        if (checkCookieArray(cname)) {
+            let len = getCookie(cname + '.length');
+            for (let i = 0; i < len; i++) {
+                setCookie(cname + '.' + i.toString(), '');
+            }
+            setCookie(cname + '.length', '');
+        }
+        setCookie(cname, "array", exdays);
+        setCookie((cname + ".length"), carray.length, exdays);
+        for (let i = 0; i < carray.length; i++) {
+            setCookie((cname + "." + i.toString()), carray[i], exdays);
+        }
+        if (cname != 'all-cookie-names') {
+            allCookiesNames = getCookieArray('all-cookie-names');
+            for (let i = 0; i < allCookiesNames.length; i++) {
+                if (cname == allCookiesNames[i]) {
+                    return true;
+                }
+            }
+            setCookie('all-cookie-names', 'array');
+            allCookiesNames.push(cname); 
+            for (let i = 0; i < allCookiesNames.length; i++) {
+                setCookie('all-cookie-names.' + i.toString(), allCookiesNames[i]);
+            }
+            setCookie('all-cookie-names.length', allCookiesNames.length);
+        }
+    }
+}
+
+/**
+ * getCookie
+ * a function that searches for the given cookie name and returns the value.
+ * If the cookie isn't found returns ""
+ *
+ * @param cname - name of cookie
+ */
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            let cvalue = c.substring(name.length, c.length);
+            cvalue = cvalue.toString();
+            cvalue = cvalue.replaceAll('}', '=');
+            cvalue = cvalue.replaceAll('{', ';');
+            return cvalue;
+        }
+    }
+    return "";
+}
+
+/**
+ * getCookieArray
+ * A function that returns an array of values from cookies based
+ * on the given cname.
+ *
+ * @param cname - Name of array
+ */
+function getCookieArray(cname) {
+    let blankArray = [];
+    if (checkCookieArray(cname)) {
+        let size = getCookie(cname + ".length");
+        for (let i = 0; i < size; i++) {
+            blankArray.push(getCookie(cname + "." + i.toString()));
+        }
+    }
+    return blankArray;
+}
+
+/**
+ * checkCookie
+ * A function that returns whether the cookie exists.
+ *
+ * @param cname - Name of cookie
+ */
+function checkCookie(cname) {
+    if (getCookie(cname) != "") {
+        return true;
     }
     return false;
 }
 
 /**
- * checkURL
- * function that checks to see if the given url is in the page url.
+ * checkCookie
+ * A function that returns whether the cookie is an array.
  *
- * @param url - String
- * @return Boolean
+ * @param cname - Name of cookie
  */
-function checkURL(url) {
-    return document.location.href.includes(url);
-}
-
-/**
- * setField
- * function that will  update a field by either giving it text of by checking the box.
- * The etype is a string param that should either be 'input' for a text field or 'click'
- * for a check box. The text param is for if the element is a text field text will
- * will be a string for the text wanting to be put in the textfield. If
- * element is given an Element Object it will search within that Element to find the
- * element wanted.
- *
- * @param selector - Element Object or String CSS Selector
- * @param etype - Either 'input' or 'click'
- * @param text - Either String or Boolean
- * @param element - Element Object to search in
- */
-function setField(selector, text, element=false) {
-    var el = findSingle(selector, element);
-    el.click();
-    el.val(text);
-}
-
-/**
- * addTag
- * function that creates an element on the page at a given location and
- * appends that element then returns the created the element.
- *
- * @param loc - element to prepend to.
- * @param tag - the tag of the element
- * @param id - id of new element
- * @param el_class - class of new element
- * @param text - the inner text of the new element
- */
-function addTag(loc, tag, id='', el_class='', text='', style='') {
-    loc = findSingle(loc);
-    var element = $("<" + tag + "/>", {
-        "id": id,
-        "class": el_class,
-        "text": text
-    });
-    if (style != '') {
-        element.css(style);
-    }
-    loc.append(element);
-    return element;
-}
-
-/**
- * replaceClass
- * function that replaces a specific class of an element.
- * 
- * @param loc - element to change class of.
- * @param org_class_name - the original class name to be replaced
- * @param new_class_name - the new class name to replace the original one
- */
-function replaceClass(loc, org_class_name, new_class_name) {
-    loc = findSingle(loc);
-    if (loc.hasClass(org_class_name)) {
-        loc.removeClass(org_class_name);
-        loc.addClass(new_class_name);
-    }
-}
-
-/**
- * itemInArray
- * function that checks to see if a given item matches one in an array.
- * 
- * @param item - The object to check to see if it exists in array.
- * @param array - The array to check against.
- * @return Bool
- */
-function itemInArray(item, array) {
-    for (var i = 0; i < array.length; i++) {
-        if (item == array[i]) {
-            return true;
-        }
+function checkCookieArray(cname) {
+    if (getCookie(cname) == 'array') {
+        return true;
     }
     return false;
 }
-/**
- * createDropdown
- * function that creates a select dropdown list given a location and list of
- * strings for the title of the options. The value and title will be the same
- * value. ID and el_class are for the class of the select element.
- *
- * @param loc - Element to prepend to.
- * @param items - the array of strings used for the list
- * @param id - id of the select element
- * @param el_class - the class of the select element
- */
-function createDropdown(loc, items, id='', el_class='') {
-    var text = '';
-    items.forEach(function(item) {
-        text += '<option value="' + item + '">' + item + '</option>';
-    });
-    var dropdown = addTag(loc, 'select', id, el_class);
-    dropdown.add(text);
-}
-
-/**
- * createEmptyTable
- * Function creates an empty Table in accela style and returns the table 
- * that was created.
- * 
- * @param loc - element to append to.
- * @param id - id of new table
- * @param el_class - class of new table
- * @param width - width attribute of new table
- * @param ignore - ignore attributte of new table
- * @return Tag Element - New table
- */
-function createEmptyTable(loc, id='', el_class='', width='', ignore='') {
-    var table = createTagAppend(loc, "table", id, el_class);
-    if (width != '') {
-        table.setAttribute("width", width);
-    }
-    if (ignore != '') {
-        table.setAttribute("ignore", ignore);
-    }
-    table.setAttribute("border", "0");
-    table.setAttribute("cellpadding", "0");
-    table.setAttribute("cellspacing", "0");
-    var tbody = createTagAppend(table, "tbody");
-    var tr = createTagAppend(tbody, "tr");
-    createTagAppend(tr, "td");
-    return table;
-}
-
-/**
- * runAngularTrigger
- * function that gets around scope issues with chrome extensions.
- * adds the script that needs to be run to trigger an angular trigger on the page.
- * then removes the code.
- *
- * @param css - css selector inside the angular.element call
- * @param trigger - the name of the trigger in .triggerHandler
- */
-function runAngularTrigger(css, trigger) {
-    var code = "angular.element('" + css + "').triggerHandler('" + trigger + "');";
-    addTag(findSingle('body'), 'script', 'angular', '', code).nodeType='text/javascript';
-    $('#angular').remove();
-}
-
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
-};
